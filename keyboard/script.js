f2bd43af54d7bccc083118e47cf1207531741026
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-05-17 22:02:59 +0530

    Update script.js

--- keyboard/script.js
+++ keyboard/script.js
@@ -1,5 +1,19 @@
+// document.getElementById('blinkButton').addEventListener('click', function() {
+//   let box = document.getElementById('box');
+//   box.classList.add('blink');
+
+//   setTimeout(function() {
+//       box.classList.remove('blink');
+//   }, 500); // Blinking duration (500ms)
+// });
+
+
 const inputElement = document.querySelector('input');
+const qKey = document.querySelector('.q');
 
 inputElement.addEventListener("keydown", (e) => {
   console.log(e.key);
+  if(e.key===q){
+    document.body.style.setProperty('background-color',rgb(23, 64, 0));
+  }
 });
