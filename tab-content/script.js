4c6fdfc8de723bf483acaf78ee5b2d97ee2e4f1e
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-05-19 20:17:56 +0530

    Add files via upload

---
+++ tab-content/script.js
@@ -0,0 +1,21 @@
+const navElement = document.querySelector('nav');
+const navbarLinks = navElement.querySelectorAll('a');
+const sectionElements = document.querySelectorAll('section');
+
+const removeActiveLinks = () => {
+  navbarLinks.forEach(link => link.parentElement.classList.remove('active'));
+};
+
+const hideSections = () =>{
+  sectionElements.forEach(section => section.classList.add('hidden'));
+};
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    removeActiveLinks();
+    hideSections();
+    link.parentElement.classList.add('active');
+    const sectionElements = document.querySelector(link.hash);//#section link
+    sectionElements.classList.remove('hidden')
+  });
+})
