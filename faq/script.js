dcadc1f944e6fea8d778b8d070bc5bff4b951604
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-05-20 21:35:28 +0530

    Add files via upload

---
+++ faq/script.js
@@ -0,0 +1,21 @@
+const dataArray = [
+  {
+    title: 'Why is JavaScript cool?',
+    detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto animi minus quis rerum, aliquam aspernatur magnam veritatis hic non iure minima molestiae vitae libero distinctio sunt corrupti voluptatum? Quaerat.'
+  },
+  {
+    title: 'What is javaScript?',
+    detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto animi minus quis rerum, aliquam aspernatur magnam veritatis hic non iure minima molestiae vitae libero distinctio sunt corrupti voluptatum? Quaerat.'
+  },
+  {
+    title: 'How can I learn JavaScript?',
+    detail: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto animi minus quis rerum, aliquam aspernatur magnam veritatis hic non iure minima molestiae vitae libero distinctio sunt corrupti voluptatum? Quaerat.'
+  }
+];
+
+const makeHTML = data => {
+  return `<details>
+    <summary>${data.title}</summary>
+    <p>${data.detail}</p>
+  </details>`
+}
