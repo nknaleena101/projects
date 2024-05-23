572e103ea66ecb1571186af9593d5f5a41626341
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-05-23 20:36:10 +0530

    Update script.js

--- testimonials/script.js
+++ testimonials/script.js
@@ -2,10 +2,34 @@ const testimonails= [
   {
     author: {
       name: 'Gabriel Moore',
-      Image: 'author-01.jpg',
+      Image: 'img/author-01.jpg',
     },
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi repellat asperiores nihil tenetur, optio aut aperiam veritatis officia ipsum. Soluta repellendus ipsam quidem accusamus? Dolor quisquam repellat sit perspiciatis.',
-    date: '23rd of may',
+    date: '23rd of May',
+  },
+  {
+    author: {
+      name: 'Toney Fergason',
+      Image: 'img/author-02.jpg',
+    },
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi repellat asperiores nihil tenetur, optio aut aperiam veritatis officia ipsum. Soluta repellendus ipsam quidem accusamus? Dolor quisquam repellat sit perspiciatis.',
+    date: '25rd of May',
+  },
+  {
+    author: {
+      name: 'Jackie Oliver',
+      Image: 'img/author-03.jpg',
+    },
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi repellat asperiores nihil tenetur, optio aut aperiam veritatis officia ipsum. Soluta repellendus ipsam quidem accusamus? Dolor quisquam repellat sit perspiciatis.',
+    date: '2nd of June',
+  },
+  {
+    author: {
+      name: 'Pauline Carter',
+      Image: 'img/author-04.jpg',
+    },
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi repellat asperiores nihil tenetur, optio aut aperiam veritatis officia ipsum. Soluta repellendus ipsam quidem accusamus? Dolor quisquam repellat sit perspiciatis.',
+    date: '9th of June',
   }
 ];
 
@@ -13,9 +37,40 @@ const containerElement = document.getElementById('testimonails-container');
 
 const makeTestimonialCard = testimonail => {
   return `<div class="testimonail-card">
-  <img src="${testimonails.author.Image}">
-  <h2>${testimonails.author.name}</h2>
-  <p>${testimonails.text}</p>
-  <date>Written on ${testimonails.date}</date>
+  <img src="${testimonail.author.Image}">
+  <h2>${testimonail.author.name}</h2>
+  <p>${testimonail.text}</p>
+  <date>Written on ${testimonail.date}</date>
   </div>`
+};
+
+let currentTestimonial = 0;
+
+const naxtTestimonial = () => {
+  if(currentTestimonial < testimonails.length - 1){
+    currentTestimonial += 1;
+    updatePage();
+  }
+};
+
+const prevTestimonial = () => {
+  if(currentTestimonial > 0){
+    currentTestimonial -= 1;
+    updatePage();
+  }
 }
+
+const updatePage = () => {
+  let markup = makeTestimonialCard(testimonails[currentTestimonial]);
+
+  if(testimonails.length > 1){
+    markup += `<nav>
+      <button onclick="prevTestimonial()">Previous</button>
+      <button onclick="naxtTestimonial()">Next</button>
+    </nav>`
+  }
+
+  containerElement.innerHTML = markup;
+};
+
+updatePage();
