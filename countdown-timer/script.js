3d4f6e78e50b2056cff39662cddf9f01d8ef3b92
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-05-25 20:24:13 +0530

    Add files via upload

---
+++ countdown-timer/script.js
@@ -0,0 +1,12 @@
+const hoursInputElement = document.getElementById("hoursInput");
+const minutesInputElement = document.getElementById("minutesInput");
+const secondsInputElement = document.getElementById("secondsInput");
+
+const hoursOutputElement = document.getElementById("hoursOutput");
+const minutesOutputElement = document.getElementById("minutesOutput");
+const secondsOutputElement = document.getElementById("secondsOutput");
+
+const startTimerButton = document.getElementById("startTimer");
+
+let targetTime;
+let timerInterval;
