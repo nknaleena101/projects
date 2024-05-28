3cf47acba8e007348235bc0bcea9d631d4fa1fba
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-05-28 20:06:32 +0530

    Update script.js

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -14,5 +14,10 @@ let timerInterval;
 const updateTimer = () => {
   if(targetTime){
     const differenceInSeconds = Math.floor( targetTime - Date.now() )/1000;     
+
+    const hours = Math.floor(differenceInSeconds / 3600);
+    const minutes = Math.floor(differenceInSeconds / 60) % 60;
+    const seconds = Math.floor(differenceInSeconds / 60);
+    //watch 1.59.00
   }
 }
