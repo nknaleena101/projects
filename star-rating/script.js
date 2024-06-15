0361912d6b8782a1a8dbdab74579023b57f8cd89
Naleena Kumarasinghe <40813959+nknaleena101@users.noreply.github.com>
2024-06-15 20:26:19 +0530

    Update script.js

--- star-rating/script.js
+++ star-rating/script.js
@@ -9,4 +9,30 @@ const storedQuestions = JSON.parse(localStorage.get("storedQuestions"));
 
 const questions = storedQuestions || initialQuestions;
 
-const ratingsElemant = document.getElementById("ratings");
+const makeStarRating = questions => {
+  const container = document.createElement('div');
+  const label = document.createElement('label');
+  label.textContent = questions.label;
+  container.appendChild(label);
+  container.appendChild(makeStars(5, questions));
+
+  return container;
+}
+
+const makeStars = (maxValue, questions) =>{
+  const starContainer = document.createElement('div');
+
+  for(let starPosition = 1; starPosition <= maxValue; starPosition++){
+    const starElement = document.createElement('span');
+    starContainer.appendChild(starElement);
+
+    if(starPosition<=questions.rating){
+      starElement.classList.add('filled');
+    }else{
+      starElement.classList.add('empty');
+    }
+  }
+  return starContainer;
+}
+
+//2.23.25
